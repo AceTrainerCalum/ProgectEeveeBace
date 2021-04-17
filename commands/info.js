8bcc2ae1105303792760488c827e98c8826e6855
AceTrainerCalum <calum.pulham@gmail.com>
2021-04-17 11:16:51 +0100

    comment

--- commands/info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'info',
-    aliases: ['infomation','i'],
-    description: "this is a info command",
-    async execute(Client, message, args, discord){
-        message.channel.send('this is the info page --- more to come --- ');
-    }
-}
--- commands/todo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'todo',
-    description: "this is a todo command",
-    async execute(Client, message, args, discord){
-        message.channel.send('this is the todo page --- more to come --- ');
-    }
-}
